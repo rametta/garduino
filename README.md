5cfb32885619203f86b2cd06e2618fb7289ed408
Jason <jason@astucemedia.com>
2017-07-02 11:25:43 -0400

    setup endpoints for accessing garden data

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Home Garduino
 
+## Start
+To start the server, make sure there is a client side build in `/client/dist` if there isn't, run `ng build --prod` inside of `/client` with the angular CLI. Once that is built, go into `/` and run npm start to start the server. The server holds the API, serves the static files for the client and connects/queries the mongo database for data.
+
 ## Client
 
 Client built on Angular 4.
 
 ## Server
 
-API server built with Node.
+API server built with Node, ExpressJS.
 
 ## Data
 
-Data stored in MongoDB.
+Data stored in MongoDB and Mongoose.
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const helmet = require('helmet');
 const compression = require('compression');
+const bodyParser = require('body-parser');
 
 const app = express();
+const router = express.Router();
 const v4 = '/client/dist';
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
   .use(helmet()) // Applies security precautions
   .use(compression()) // gzip compress
-
   .use('/', express.static(__dirname + v4, { maxAge: '20d' }))
-
-  .get('/ApiHost', (req, res) => {
-    res.json({ ApiHost: proxyUrl });
-  })
+  .use('/api', router)
 
   .use('*', (req, res, next) => {
     res.sendFile(__dirname + v4 + '/index.html');
@@ -22,4 +22,56 @@ app
 
   .listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
+  });
+
+router
+  .get('/', (req, res) => {
+    const BASE = `http://localhost:${port}`;
+
+    res.json({
+      welcome: 'Welcome to the Rametta Garduino API',
+      routes: [
+        {
+          type: 'GET',
+          route: `${BASE}/api/gardens`,
+          returns: 'An array of garden objects'
+        },
+        {
+          type: 'GET',
+          route: `${BASE}/api/garden/{ID}`,
+          returns: 'One garden object'
+        },
+        {
+          type: 'POST',
+          route: `${BASE}/api/garden/{ID}`,
+          returns: 'Insert/update a new/existing garden'
+        },
+        {
+          type: 'DELETE',
+          route: `${BASE}/api/garden/{ID}`,
+          returns: 'Delete a garden object'
+        }
+      ]
+    });
+  })
+
+  .get('/gardens', (req, res) => {
+    res.json([
+      {
+        _id: 1,
+        date: new Date().toISOString(),
+        temperature: 3.2233423,
+        humidity: 1.123235,
+        moisture: 34.234,
+        light: 4
+      },
+      {
+        _id: 2,
+        date: new Date().toISOString(),
+        temperature: 3.2233423,
+        humidity: 1.123235,
+        moisture: 34.234,
+        light: 5
+      }
+    ]);
   });
